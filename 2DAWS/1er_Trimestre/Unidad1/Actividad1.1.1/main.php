c819f0f4efe449abae9aa4e2cf6633fd7fb5c379
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-09-29 13:38:53 +0200

    Actividad 1.1.1 Finalizada

--- 2DAWS/1er_Trimestre/Unidad1/Actividad1.1.1/main.php
+++ 2DAWS/1er_Trimestre/Unidad1/Actividad1.1.1/main.php
@@ -28,7 +28,7 @@
         // SI LE DAN A ENVIAR
         if(isset($_POST['enviar'])){
 
-            // SI NOMBRE ESTÁ VACÍO
+            // SI NOMBRE ESTÁ VACÍO MUESTRA FORM + ERROR
             if(empty($_POST['nombre'])){
     ?>
                 <div class="caja">
@@ -46,19 +46,40 @@
                 <h1>Por favor, debe rellenar el campo de nombre</h1>
         
             <?php
+            // SI MODULO ESTÁ VACÍO MUESTRA FORM + ERROR
+            } elseif (empty($_POST['modulos'])){
+            ?>
+                <div class="caja">
+                <img src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png">
+                <form method="POST" action="">
+                    <p>Su nombre: </p>
+                    <input type="text" name="nombre" placeholder="Teléfono, usuario o correo electrónico"><br/>
+                    <p>Asignaturas que cursa: </p>
+                    <input type="checkbox" name="modulos[]" value="DWES">
+                    Desarrollo Web en Entorno Servidor<br/>
+                    <input type="checkbox" name="modulos[]" value="DWEC">
+                    Desarrollo Web en Entorno Cliente<br/>
+                    <input type="submit" name="enviar">
+                </form>
+                <h1>Por favor, debe rellenar el campo de módulos</h1>
+            <?php
+            // SI AMBOS ESTÁN RELLENOS MUESTRA EL RESULTADO
+            } else {
+            ?>
+            <?php
+                $nombre = $_POST['nombre'];
+                $modulos = $_POST['modulos'];
+
+                print "Nombre del alumno: ".$nombre."<br/>";
+
+                foreach($modulos as $modulo ){
+                    print "Cursa el siguiente módulo ".$modulo."<br/>";
+                }
             }
             ?>
     </div> 
     <?php
-            $nombre = $_POST['nombre'];
-            $modulos = $_POST['modulos'];
-
-            print "Nombre del alumno: ".$nombre."<br/>";
-
-            foreach($modulos as $modulo ){
-                print "Cursa el siguiente módulo ".$modulo."<br/>";
-            }
-
+        // SI NO LE DAN A ENVIAR MUESTRA EL FORM
         } else {
     ?>
             <div class="caja">
