c216d754b8d95f45c818ab8b665417815cdd08e8
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-10-03 13:45:37 +0200

    Cuenta atrás vuelos

--- 2DAWS/1er_Trimestre/Unidad1/Actividad3/main.php
+++ 2DAWS/1er_Trimestre/Unidad1/Actividad3/main.php
@@ -11,60 +11,62 @@
 
 <body>
 
-    <?php
-    // Si está bien relleno muestra resultado
-    if (!empty($_POST['fechaActual']) && !empty($_POST['fechaVuelo'])) {
 
-        $fechaactual = new DateTime($_POST['fechaActual']);
-        $fechavuelo = new DateTime($_POST['fechaVuelo']);
-
-        $interval = $fechaactual->diff($fechavuelo);
-
-        echo "difference " . $interval->y . " years, " . $interval->m . " months, " . $interval->d . " days ";
-        //echo "<h3>La diferencia es de ".$diff->format("%d")."</h3>";
-
-        if (empty($_POST['horaActual'])){
-            echo '<h4> Usted ha dejado vacío el campo de hora actual</h4>';
-            
-        } elseif (empty($_POST['horaVuelo'])){
-            echo '<h4> Usted ha dejado vacío el campo de hora vuelo</h4>';
-
-        } else {
-            $horaActual = new DateTime($_POST['horaActual']);
-            $horaVuelo = new DateTime($_POST['horaVuelo']);
-
-            $diferenciaHoras = date_diff($horaActual, $horaVuelo);
-            $diferenciaHoras = $diferenciaHoras->format('%H:%i:%s');
-
-            echo '<h4>'.$diferenciaHoras.' estas son las horas restantes</h4>'; 
-        }
-
-    } else {
-    ?>
-        <div class="caja">
-            <div class="form">
-                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <div class="caja">
+        <div class="form">
+            <h2>Calcula el tiempo restante para tu vuelo</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                <div class="columnaizq">
                     <label for="hora">Introduce la fecha actual</label>
                     <input type="date" name="fechaActual">
                     <br>
                     <label for="hora">Introduce la hora actual</label>
                     <input type="time" name="horaActual">
                     <br>
-                    <br>
+                </div>
+
+                <div class="columnader">
                     <label for="hora">Introduce la fecha vuelo</label>
                     <input type="date" name="fechaVuelo">
                     <br>
                     <label for="hora">Introduce la hora del vuelo</label>
                     <input type="time" name="horaVuelo">
                     <br>
-                    <input type="submit" name="enviar">
-                </form>
-            </div>
-        </div>
+                </div>
 
-    <?php
-    }
-    ?>
+                <input style="margin-bottom: 20px;" type="submit" name="enviar" value="Enviar consulta" />
+            </form>
+
+            <?php
+            if (!empty($_POST['fechaActual']) && !empty($_POST['fechaVuelo'])) {
+
+                $fechaactual = new DateTime($_POST['fechaActual']);
+                $fechavuelo = new DateTime($_POST['fechaVuelo']);
+
+                $interval = $fechaactual->diff($fechavuelo);
+
+                echo "<h4>Para que tu avión despegue le quedan <br>". $interval->m . " meses y " . $interval->d . " días.</h4> ";
+
+                if (empty($_POST['horaActual'])) {
+                    echo '<h4> Usted ha dejado vacío el campo de hora actual</h4>';
+                } elseif (empty($_POST['horaVuelo'])) {
+                    echo '<h4> Usted ha dejado vacío el campo de hora vuelo</h4>';
+                } else {
+                    $horaActual = new DateTime($_POST['horaActual']);
+                    $horaVuelo = new DateTime($_POST['horaVuelo']);
+
+                    $diferenciaHoras = date_diff($horaActual, $horaVuelo);
+                    $diferenciaHorash = $diferenciaHoras->format('%H');
+                    $diferenciaHorasm = $diferenciaHoras->format('%i');
+
+                    echo '<h3>' . $diferenciaHorash . ' horas y '.$diferenciaHorasm.' minutos.</h3>';
+                }
+            }
+            ?>
+        </div>
+    </div>
+
+
 
 
 </body>
