ff408641dc54f0b1c01168f7bb0a7b9d92c08cc8
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-10-19 19:23:28 +0200

    Actu php

--- 2DAWS/1er_Trimestre/Unidad2/Actividad1/index.php
+++ 2DAWS/1er_Trimestre/Unidad2/Actividad1/index.php
@@ -11,14 +11,14 @@
             margin: auto;
         }
 
-        .registroContainer{
+        .registroContainer {
             border-right: solid 1px black;
             display: inline-block;
             margin: 1rem;
             padding: 0.5rem;
         }
 
-        .updateContainer{
+        .updateContainer {
             margin: 1rem;
             display: inline-block;
         }
@@ -26,46 +26,45 @@
 </head>
 
 <body>
-<?php
-@ $conexion = new mysqli('localhost', 'root', '', 'mysql_employees');
-
-$error = $conexion->connect_errno;
-
-if ($error != null) {
-    echo "<p>Error $error conectando a la base de datos: $dwes->connect_error</p>";
-    exit();
-} else {
-
-    // Este código se ejecuta si la conexión con bbdd ha sido buena
-    // 1.- Recogida y gestión de datos del POST
-
-    // 2.- Generacion e impresión del resultado
-
-    $resultados = $conexion->query('SELECT * FROM department');
-    while ($departamento = $resultados->fetch_array()) {
-        
-    }
-
-?>
-    <div class="mainContainer">
-        <h1>Departamentos</h1>
-
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-            <div class="addContainer">
-            <input type="submit" value="+" name="add_button"><input type="text" value="" placeholder="Nombre nuevo departamento" name="new_department_name"><br>
-            </div>
-            <div class="registroContainer">
-                <input type="submit" value="x" name="delete_001"><input type="text" value="marketing" name="name_001"><br>
-                <input type="submit" value="x" name="delete_002"><input type="text" value="marketing" name="name_002"><br>
-            </div>
-            <div class="updateContainer">
-                <input type="submit" value="Actualizar">
-            </div>
-
-        </form>
-    </div>
     <?php
-}
+    @$conexion = new mysqli('localhost', 'root', '', 'mysql_employees');
+
+    $error = $conexion->connect_errno;
+
+    if ($error != null) {
+        echo "<p>Error $error conectando a la base de datos: $dwes->connect_error</p>";
+        exit();
+    } else {
+
+        // Este código se ejecuta si la conexión con bbdd ha sido buena
+        // 1.- Recogida y gestión de datos del POST
+
+        // 2.- Generacion e impresión del resultado
+
+        $resultados = $conexion->query('SELECT * FROM department');
+    ?>
+        <div class="mainContainer">
+            <h1>Departamentos</h1>
+
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+                <div class="addContainer">
+                    <input type="submit" value="+" name="add_button"><input type="text" value="" placeholder="Nombre nuevo departamento" name="new_department_name"><br>
+                </div>
+                <div class="registroContainer">
+                    <?php while ($departamento = $resultados->fetch_array()) { ?>
+                        <input type="submit" value="x" name="delete_<?php echo $departamento['dept_no']; ?>"><input type="text" value="<?php echo $departamento['dept_name']; ?>" name="name_<?php echo $departamento['dept_no']; ?>"><br>
+                    <?php
+                    }
+                    ?>
+                </div>
+                <div class="updateContainer">
+                    <input type="submit" value="Actualizar">
+                </div>
+
+            </form>
+        </div>
+    <?php
+    }
     $conexion->close();
     ?>
 </body>
