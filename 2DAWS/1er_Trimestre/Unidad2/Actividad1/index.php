5aa4648a95ee51c379b3cb1ab9447f6e0a4737a5
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-10-19 18:23:40 +0200

    Cambios php

--- 2DAWS/1er_Trimestre/Unidad2/Actividad1/index.php
+++ 2DAWS/1er_Trimestre/Unidad2/Actividad1/index.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Departamento - Operaciones CRUD</title>
+    <title>Departamentos - Operaciones CRUD</title>
     <style>
         .mainContainer {
             margin: auto;
         }
-        .registrosContainer{
+
+        .registroContainer{
             border-right: solid 1px black;
             display: inline-block;
             margin: 1rem;
@@ -25,52 +26,38 @@
 </head>
 
 <body>
-
 <?php
+@ $conexion = new mysqli('localhost', 'root', '', 'mysql_employees');
 
-        @ $conexion = new mysqli('localhost', 'root', '', 'employees');
+$error = $conexion->connect_errno;
 
-        $error = $conexion->connect_errno;
+if ($error != null) {
+    echo "<p>Error $error conectando a la base de datos: $dwes->connect_error</p>";
+    exit();
+} else {
 
-        if($error != null){
+}
 ?>
-            <!-- Esto es el HTML si hay error -->
-            <p>Error de conexion a la base de datos. <?php echo $conexion->connect_error; ?> Vuelve en un rato.</p>
-            
-<?php
-            exit();
-        }else{
-            //Este código se ejecuta si la conexion a la base de datos ha ido bien.
-        }
-
-?>
-
     <div class="mainContainer">
         <h1>Departamentos</h1>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
-        <div class="addContainer">
-            <input type="submit" value="+" name="add_button"> <input type="text" name="new_department_name" placeholder="Nombre de nuevo departamento">
-        </div> 
-
-            <div class="registrosContainer">
-
-                <input type="submit" value="x" name="delete_001"> <input type="text" name="name_001" value="Marketing"> <br>
-                <input type="submit" value="x" name="delete_002"> <input type="text" name="name_002" value="Sales"> <br>
-
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <div class="addContainer">
+            <input type="submit" value="+" name="add_button"><input type="text" value="" placeholder="Nombre nuevo departamento" name="new_department_name"><br>
             </div>
-
+            <div class="registroContainer">
+                <input type="submit" value="x" name="delete_001"><input type="text" value="marketing" name="name_001"><br>
+                <input type="submit" value="x" name="delete_002"><input type="text" value="marketing" name="name_002"><br>
+            </div>
             <div class="updateContainer">
-                <input type="submit" value="Actualizar registros" name="update_button">
+                <input type="submit" value="Actualizar">
             </div>
 
         </form>
     </div>
-
-    <?php 
-        $conexion->close();
+    <?php
+    $conexion->close();
     ?>
-
 </body>
 
 </html>
