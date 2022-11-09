b44c942008de0eb0f5ce78a85e1dd24927741fbd
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-11-09 18:19:43 +0100

    Clase DAWC

---
+++ 2DAWC/Unidad2/Actividades/ObjetoDocument/Actividad2.js
@@ -0,0 +1,4 @@
+function hacerCambios() {
+    document.getElementById("parrafo2").innerHTML = "Éste es el nuevo contenido del segundo párrafo";
+    document.getElementById("parrafo3").innerHTML = "Éste es el nuevo contenido del <strong>tercer párrafo</strong>.";
+}
