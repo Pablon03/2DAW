9d431d1adda9736c49734f3ae9fd27844bf5d20e
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-11-10 16:38:50 +0100

    Practicas

--- 2DAWS/1er_Trimestre/Unidad2/ActividadesVarias/PracticasExamen/MatrizColoreada/abrirBBDDLinux.php
+++ 2DAWS/1er_Trimestre/Unidad2/ActividadesVarias/PracticasExamen/MatrizColoreada/abrirBBDDLinux.php
@@ -7,57 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $dbHost = 'localhost';
-        $dbUser = 'root';
-        $dbPass = '';
-        $dbName = 'world';
 
-        $linkHost = mysqli_connect($dbHost, $dbUser, $dbPass);
-        $dbLink = mysqli_select_db($linkHost, $dbName);
-        $conexion = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
-
-    ?>
-        <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
-            <input type="submit" name="crearBBDD" value="Crear Base de Datos">
-            <input type="submit" name="crearCiudad" value="Crear Ciudad">
-            <input type="submit" name="mostrarCiudades" value="Mostrar Ciudades">
-            <input type="submit" name="actualizarCiudad" value="Actualizar Ciudad">
-            <input type="submit" name="borrarCiudad" value="Borrar Ciudad">
-        </form>
     <?php
-            if (isset($_POST['crearBBDD']) && !empty($_POST['crearBBDD'])) {
-                if (!$linkHost) {
-                    echo "<h1>Error, datos incorrectos del servidor</h1>";
-                } elseif ($dbLink) {
-                    echo "<h1>Error, la base de datos estaba ya creada</h1>";
-                } else {
-                    exec('cd ./bbdd && /opt/lampp/htdocs/{bbdd} -u root < world.sql');
-                }
-            } elseif (isset($_POST['crearCiudad']) && !empty($_POST['crearCiudad'])) {
+        if (isset($_POST['crear'])) {
+            exec('./bbdd && /opt/lamp/bin/mysql -u root < world.sql');
+        }
     ?>
-            <form action="./mostrarCiudades.php" method="post">
-                <input type="text" name="nombreCiudad" value="Nombre nueva ciudad">
-                <input type="submit" name="nuevaCiudad" value="Añadir Ciudad">
-            </form>
-    <?php
-            } elseif (isset($_POST['mostrarCiudades']) && !empty($_POST['mostrarCiudades'])) {
-                $consultaCompleta = "SELECT * FROM city ORDER BY ID DESC LIMIT 10";
-                $consulta = $conexion->query($consultaCompleta);
-
-                echo "<table>";
-                while ($fila = mysqli_fetch_assoc($consulta)) {
-                    // print_r($fila);
-                ?>
-                    <tr>    
-                        <td><?php echo $fila['ID'] ?></td>
-                        <td><?php echo $fila['Name']?></td>
-                    </tr>
-                <?php
-                }
-                echo "</table>";
-            }
-    $conexion->close();
-    ?>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="submit" name="crear" value="crearbbdd"/>
+    </form>
 </body>
 </html>
