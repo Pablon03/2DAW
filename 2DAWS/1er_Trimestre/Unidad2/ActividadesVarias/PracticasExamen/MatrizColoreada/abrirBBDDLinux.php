f375b85c0a54debbbed4213b77c360b3b2fa0182
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-11-10 16:52:07 +0100

    hola

--- 2DAWS/1er_Trimestre/Unidad2/ActividadesVarias/PracticasExamen/MatrizColoreada/abrirBBDDLinux.php
+++ 2DAWS/1er_Trimestre/Unidad2/ActividadesVarias/PracticasExamen/MatrizColoreada/abrirBBDDLinux.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>hola</title>
 </head>
 <body>
 
     <?php
+    print_r($_POST);
+    echo "gola";
         if (isset($_POST['crear'])) {
-            if(exec('./bbdd && /opt/lamp/bin/mysql -u root < world.sql')){
+            if(exec('cd ./bbdd && /opt/lampp/bin/mysql -u root < world.sql')) {
                 echo "Creado bien";
             } else {
                 echo "NO se ha creado";
             }
         }
     ?>
-    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+    <form action="abrirBBDDLinux.php" method="POST">
     <input type="submit" name="crear" value="crearbbdd"/>
     </form>
 </body>
