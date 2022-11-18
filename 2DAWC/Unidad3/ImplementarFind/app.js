b2efef14e14b9b9cc02ed4bd0f6a867cc3f47491
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2022-11-18 19:19:46 +0100

    DWC

---
+++ 2DAWC/Unidad3/ImplementarFind/app.js
@@ -0,0 +1,23 @@
+function find(array, func_callback) {
+    let res = null;
+    for (let i = 0; i < array.length && !res; i++) {
+        if (func_callback(array[i], i, array)) {
+            res = array[i];
+        }
+    }
+    return res;
+}
+
+function func_callback(elem, indice, array) {
+    let res = false;
+for (let j = indice + 1; j < array.length && !res; j++) {
+        if (array[j] === elem) {
+            res = true;
+        }
+    }
+    return res;
+}
+
+const elems = [3, 6, 9, 7, 7, 4, 9];
+const numero = find(elems, func_callback);
+console.log(numero);
