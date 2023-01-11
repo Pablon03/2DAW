f15fe296f829c983f8af1b1847012dedc3808a94
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2023-01-11 17:59:18 +0100

    dwec

--- 2DAWC/Unidad4/testForm/js/app.js
+++ 2DAWC/Unidad4/testForm/js/app.js
@@ -1,1 +1,7 @@
 let formulario = document.getElementById("form");
+
+let form1 = document.formulario.email;
+console.log(form1);
+
+let form2 = document.forms["formulario1"].elements["nombre"];
+console.log(form2);
