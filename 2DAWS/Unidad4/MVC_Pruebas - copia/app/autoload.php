21b0f7df00a51661c04e23bfa59c7c4dfc909e1d
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2023-01-18 16:44:45 +0100

    clientes

--- 2DAWS/Unidad4/MVC_Pruebas - copia/app/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DS', DIRECTORY_SEPARATOR);
-
-function app_autoload($classname){
-    $classname = __DIR__ . DS . str_replace('\\', DS, $classname). '.php';
-    require_once $classname;
-}
-spl_autoload_register('app_autoload');
--- 2DAWS/Unidad4/MVC_Pruebas - copia/app/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('RUTA_APP', dirname(dirname(__FILE__)));
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'app_pokemons');
