205e8973a3cb5fdb38bfaffd03ec29a0e0553550
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2023-02-06 20:45:05 +0100

    dwes

--- 2DAWC/Unidad5/Actividad7/js/app.js
+++ 2DAWC/Unidad5/Actividad7/js/app.js
@@ -1,43 +1,26 @@
 // Importamos la base de datos
-import {productos, categorias} from "../db/db.js";
-import {controladorCarrito, controladorBBDD} from "../js/controlador.js";
+import { productos, categorias } from "../db/db.js";
+import { controladorCarrito, controladorBBDD } from "../js/controlador.js";
 
 /////////////////////ACTIVIDAD 1/////////////////////////////
 seeAllProducts();
+function seeAllProducts() {
+  let arrayProducts = "";
+  for (let index = 0; index < productos.length; index++) {
+    // Obtenemos el producto y lo metemos a la función
+    // que nos mostrará el producto
+    let productNow = controladorBBDD.getProducts(index);
+    arrayProducts += seeProducts(productNow);
+  }
 
-function seeAllProducts(){
-    let arrayProducts = '';
-    for (let index = 0; index < productos.length; index++) {
-        // Obtenemos el producto y lo metemos a la función
-        // que nos mostrará el producto
-        let productNow = controladorBBDD.getProducts(index);
-        arrayProducts += seeProducts(productNow);
-
-
-        // console.log(dataBBDD);
-        // console.log("------------------------------");
-    }
-
-    const productsContainer = document.getElementById("products-container");
-    productsContainer.innerHTML = arrayProducts;
+  const productsContainer = document.getElementById("products-container");
+  productsContainer.innerHTML = arrayProducts;
 }
 
+function seeProducts(productNow) {
+  const { id, nombre, categoria, imagen, precio, vendedor, stock } = productNow;
 
-
-/**
- *  TENGO QUE PCOGE EL ID DE LA CATEGORIA DEL PRODUCTO QUE SE VA A METER
- * LA FUNCION getCategoryByID DEL CONTROLADOR SE ENCARGA
- * 
- */
-
-
-function seeProducts(productNow){
-
-    const {id, nombre, categoria, imagen, precio, vendedor, stock} = productNow;
-
-    const idCategory = controladorBBDD.getCategoryByID(categoria);
-
-    return `<article id="${id}" class="location-listing" data-categoria="${id}">
+  return `<article id="${id}" class="location-listing" data-categoria="${categoria}">
     <div class="location-image">
     <a href="#">
     <img src="${imagen}" alt="${nombre}">
@@ -55,20 +38,67 @@ function seeProducts(productNow){
     </article>`;
 }
 
+/////////////////////ACTIVIDAD 2/////////////////////////////
+seeAllCategories();
+putListeners();
 
-// <article id="007" class="location-listing" data-categoria="005">
-// <div class="location-image">
-// <a href="#">
-// <img src="res/art007.webp" alt="Placa base Asus">
-// </a>
-// </div>
-// <div class="data">
-// <h4>Placa base Asus</h4>
-// <p class="price">187,45€</p>
-// <p>Vendido por <strong>Amazonia</strong></p>
-// <p>Quedan 6 unidades</p>
-// <div class="button-container">
-// <a class="button add" href="#" target="_blank">Añadir al carrito</a>
-// </div>
-// </div>
-// </article>
+function seeAllCategories() {
+  let arrayCategories = `<form>
+    <fieldset id="filtro-categoria" name="filtro-categoria">
+    <legend>Filtros por categoría:</legend>`;
+  for (let index = 0; index < categorias.length; index++) {
+    let categoryNow = controladorBBDD.getCategories(index);
+    arrayCategories += seeCategorie(categoryNow);
+  }
+
+  arrayCategories += `</fieldset>
+    </form>`;
+
+  const categorieContainer = document.getElementById("filter-container");
+  categorieContainer.innerHTML = arrayCategories;
+}
+
+function seeCategorie(categoryNow) {
+  const { id, nombre } = categoryNow;
+
+  return `<div class="contenedor-categoria">
+    <input type="checkbox" id="${id}" name="${id}" class="category" value="${id}">
+    <label for="${id}">${nombre}</label>
+    </div>`;
+}
+
+function putListeners() {
+  document
+    .getElementById("filter-container")
+    .addEventListener("click", filterCategories, false);
+}
+
+function filterCategories(e) {
+  const checkboxElements = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
+  //
+  let arrayProductsFilter = "";
+
+  cleanProducts();
+
+  for (const checkbox of checkboxElements) {
+    let arrayProducts = controladorBBDD.filterCategoriesProducts(
+      checkbox.value
+    );
+
+    for (let index = 0; index < arrayProducts.length; index++) {
+      let productNow = controladorBBDD.getProductsByID(arrayProducts[index].id);
+      arrayProductsFilter += seeProducts(productNow);
+    }
+  }
+
+  //   cleanProducts();
+
+  //   const productsContainer = document.getElementById("products-container");
+  //   productsContainer.innerHTML = arrayProducts;
+}
+
+function cleanProducts() {
+  document.getElementById("products-container").innerHTML = "";
+}
--- 2DAWC/Unidad5/Actividad7/js/controlador.js
+++ 2DAWC/Unidad5/Actividad7/js/controlador.js
@@ -1,37 +1,24 @@
 // Importamos la base de datos
-import {productos, categorias} from "../db/db.js";
+import { productos, categorias } from "../db/db.js";
 
-export class controladorBBDD{
+export class controladorBBDD {
+  static getProducts(index) {
+    return productos[index];
+  }
 
-    static getProducts(index) {
-        return productos[index];
-    }
+  static getCategories(index) {
+    return categorias[index];
+  }
 
-    /**
-     * 
-     * @param {string} category Nombre categoria producto
-     * @returns id de la categoria
-     */
-    static getCategoryByID(category){
-        let i = 0;
-        let exit = false;
+  static filterCategoriesProducts(id) {
+    return productos.filter((product) => product.categoria === id);
+  }
 
-        let correctID = "";
-        do {
-            let categoryCheck = categorias[i];
-            console.log(categorias[0].nombre);
-            let categoryName = categoryCheck.nombre;
-
-            if(categoryName == category){
-                correctID = categoryCheck.id;
-                exit = true;
-            }
-            i++;
-        } while (!exit);
-        return correctID;
-    }
+  static getProductsByID(id) {
+    let indexDDBB = id-1;
+    // return productos[indexDDBB][id];
+    console.log(productos[indexDDBB][id])
+  }
 }
 
-export class controladorCarrito{
-
-}
+export class controladorCarrito {}
