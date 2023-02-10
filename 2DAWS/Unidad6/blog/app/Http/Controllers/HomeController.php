985a0ec2c1dc8fda54284b9bfe1c15237989379d
Pablon03 <pablonicolasmartinezborreguero@gmail.com>
2023-02-10 21:47:48 +0100

    laravel

--- 2DAWS/Unidad6/blog/app/Http/Controllers/HomeController.php
+++ 2DAWS/Unidad6/blog/app/Http/Controllers/HomeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke(){
-        return "Bienvenido a la página";
+        return view('home');
     }
 }
---
+++ 2DAWS/Unidad6/blog/resources/views/Cursos/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Ver Cursos')
+
+@section('content')
+    <h1>Bienvenido al curso: <?php echo $curso ?></h1>
+@endsection
